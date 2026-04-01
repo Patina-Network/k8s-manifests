ebabc0130f560c3333980ea9a4eb8cc5ef833c55
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-31 22:51:35 -0400

    Add github membership logic handled via Helm templates so users can be added and removed easily

---
+++ .nvim.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*/templates/*.yaml",
+	callback = function()
+		vim.bo.filetype = "helm"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "Chart.yaml",
+	callback = function()
+		vim.bo.filetype = "helm"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "values.yaml",
+	callback = function()
+		vim.bo.filetype = "helm"
+	end,
+})
